2d4f5439c4c62d47f2e112b065645ef782625f53
anandgiri009 <vivek.giri@gmail.com>
2026-05-27 15:14:19 -0500

    Connect to Snowflake and fetch fruit details
    Added connection to Snowflake and query for fruit details.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,9 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+conn = st.connection("snowflake")
+df = conn.query("SELECT * FROM garden_plants.fruits.fruit_details")
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
