a773ab7e819a65bc4574ed10e5cf918cf04e0bdc
anandgiri009 <vivek.giri@gmail.com>
2026-05-27 21:11:33 -0500

    Update Snowflake connection handling in streamlit_app
    Refactor Snowflake connection to use secrets and caching.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import snowflake.connector
+
 cnx = st.connection("snowflake")
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -19,15 +21,22 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-conn = st.connection(
-    "",
-    type="snowflake",
-    account="RQHLXVH-MWB31948",
-    user="ANANDGIRIDHAR007",
-    authenticator="externalbrowser",
-    **kwargs
-)
-df = conn.query("SELECT * FROM garden_plants.fruits.fruit_details")
+@st.cache_resource
+def get_connection():
+    return snowflake.connector.connect(
+        account=st.secrets["snowflake"]["RQHLXVH-MWB31948"],
+        user=st.secrets["snowflake"]["ANANDGIRIDHAR007"],
+        password=st.secrets["snowflake"]["Ferrari2645884"],
+        warehouse=st.secrets["snowflake"]["COMPUTE_WH"],
+        database=st.secrets["snowflake"]["GARDEN_PLANTS"],
+        schema=st.secrets["snowflake"]["FRUITS"],
+    )
+
+conn = get_connection()
+cur = conn.cursor()
+cur.execute("SELECT * FROM garden_plants.fruits.fruit_details")
+df = cur.fetch_pandas_all()
+st.dataframe(df)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
