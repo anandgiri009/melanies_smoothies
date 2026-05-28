7fe4b381f5d2311daad1de43275a1f98975a3bfd
anandgiri009 <vivek.giri@gmail.com>
2026-05-27 21:30:04 -0500

    Reverting to the original version
    Reverting to the original version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # Import python packages
 import streamlit as st
-import snowflake.connector
-
-#cnx = st.connection("snowflake")
-#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -16,27 +13,9 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 
-#session = get_active_session()
-#session = cnx.session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-
-@st.cache_resource
-def get_connection():
-    return snowflake.connector.connect(
-        account=st.secrets["snowflake"]["RQHLXVH-MWB31948"],
-        user=st.secrets["snowflake"]["ANANDGIRIDHAR007"],
-        password=st.secrets["snowflake"]["Ferrari2645884"],
-        warehouse=st.secrets["snowflake"]["COMPUTE_WH"],
-        database=st.secrets["snowflake"]["GARDEN_PLANTS"],
-        schema=st.secrets["snowflake"]["FRUITS"],
-    )
-
-conn = get_connection()
-cur = conn.cursor()
-cur.execute("SELECT * FROM garden_plants.fruits.fruit_details")
-df = cur.fetch_pandas_all()
-st.dataframe(df)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
